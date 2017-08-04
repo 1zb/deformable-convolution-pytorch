d07d62bb0e343c105583c2497bdae724c6c5f16a
Biao Zhang <zbalpha@outlook.com>
2017-08-04 21:17:19 +0800

    Create the repo.

---
+++ README.md
@@ -0,0 +1,12 @@
+# Deformable Convolutional Networks in PyTorch
+This repo is an implementation of [Deformable Convolution](https://arxiv.org/abs/1703.06211).
+Ported from author's MXNet [implementation](https://github.com/msracver/Deformable-ConvNets).
+
+# Build
+
+```
+sh make.sh
+CC=g++ python build.py
+```
+
+See `test.py` for example usage.
---
+++ test.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torch.autograd import gradcheck
+from torch.nn.modules.utils import _single, _pair
+
+from modules import ConvOffset2d
+
+num_deformable_group = 1
+
+N, inC, inH, inW = 1, 3, 512, 512
+outC, outH, outW = 4, 512, 512
+kH, kW = 3, 3
+
+conv = nn.Conv2d(inC, num_deformable_group * 2 * kH * kW, kernel_size=(kH, kW), stride=(1,1), padding=(1,1), bias=False).cuda()
+conv_offset2d = ConvOffset2d(inC, outC, (kH, kW), stride=1, padding=1).cuda()
+
+inputs = Variable(torch.randn(N, inC, inH, inW).cuda())
+offset = conv(inputs)
+output = conv_offset2d(inputs, offset)
+output.backward(output.data)
+print(output.size())
