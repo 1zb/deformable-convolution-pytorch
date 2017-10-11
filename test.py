1ea58b936e770ecb690a28c19d5e3c4098a841bc
Biao Zhang <zbalpha@outlook.com>
2017-10-11 09:42:11 +0800

    Update example for multiple deform groups

--- test.py
+++ test.py
@@ -5,20 +5,26 @@ from torch.autograd import Variable
 
 from modules import ConvOffset2d
 
-num_deformable_group = 1
+num_deformable_groups = 2
 
-N, inC, inH, inW = 1, 3, 512, 512
+N, inC, inH, inW = 1, 6, 512, 512
 outC, outH, outW = 4, 512, 512
 kH, kW = 3, 3
 
 conv = nn.Conv2d(
     inC,
-    num_deformable_group * 2 * kH * kW,
+    num_deformable_groups * 2 * kH * kW,
     kernel_size=(kH, kW),
     stride=(1, 1),
     padding=(1, 1),
     bias=False).cuda()
-conv_offset2d = ConvOffset2d(inC, outC, (kH, kW), stride=1, padding=1).cuda()
+
+conv_offset2d = ConvOffset2d(
+    inC,
+    outC, (kH, kW),
+    stride=1,
+    padding=1,
+    num_deformable_groups=num_deformable_groups).cuda()
 
 inputs = Variable(torch.randn(N, inC, inH, inW).cuda())
 offset = conv(inputs)
